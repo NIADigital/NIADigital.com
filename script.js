7d4f0370bd967657833639af4ff1694a0d501df4
NickJohn547745 <nicholas.johnson0629@gmail.com>
2019-01-11 14:20:57 -0500

    Updated files to include Vue-Material.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial.default)
+
 new Vue({
 	el: '#app',
 	data: {
